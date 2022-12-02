1e92a8a07ca9bc7c5c8377d3f3987591b36fcdcd
Jérôme Hénin <heninj@gmail.com>
2022-12-02 11:03:30 +0100

    First version of NAMD lambda-dynamics
    Save pointer to NAMD Controller in proxy
    
    this is necessary for accessing the Controller from threads other than 0
    
    Simplify accelMD call
    
    Use alch lambda value from state file
    
    Dependencies updates and fixes
    
    - distinguish f_cv_gradient and f_cv_apply_force
    - rename external cvs "driven"
    - fix bug where error messages were not informative for complex
    dependency resolution failures
    
    Lambda-dynamics improvements
    
    - add total_force_current_step feature
    - follow lambda value from back-end
    
    Remove unused variables
    
    Enable scriptedColvarforces in ABMD script
    
    Allow scripted forces on external param
    
    Print unscaled biasing force in MTS

--- src/colvarbias_abf.cpp
+++ src/colvarbias_abf.cpp
@@ -124,10 +124,11 @@ int colvarbias_abf::init(std::string const &conf)
       colvars[i]->enable(f_cv_hide_Jacobian);
     }
 
-    // If any colvar is extended-system, we need to collect the extended
-    // system gradient
-    if (colvars[i]->is_enabled(f_cv_extended_Lagrangian))
+    // If any colvar is extended-system (restrained, not driven external param), we are running eABF
+    if (colvars[i]->is_enabled(f_cv_extended_Lagrangian)
+        && !colvars[i]->is_enabled(f_cv_external)) {
       enable(f_cvb_extended);
+    }
 
     // Cannot mix and match coarse time steps with ABF because it gives
     // wrong total force averages - total force needs to be averaged over
@@ -315,18 +316,15 @@ int colvarbias_abf::update()
   size_t i;
   for (i = 0; i < num_variables(); i++) {
     bin[i] = samples->current_bin_scalar(i);
+    if (colvars[i]->is_enabled(f_cv_total_force_current_step)) {
+      force_bin[i] = bin[i];
+    }
   }
-
 
   // ***********************************************************
   // ******  ABF Part I: update the FE gradient estimate  ******
   // ***********************************************************
 
-
-  if (cvm::proxy->total_forces_same_step()) {
-    // e.g. in LAMMPS, total forces are current
-    force_bin = bin;
-  }
 
   // Share data first, so that 2d/3d PMF is refreshed using new data for mw-pABF.
   // shared_on can be true with shared_freq 0 if we are sharing via script
@@ -379,11 +377,10 @@ int colvarbias_abf::update()
     }
   }
 
-  if (!(cvm::proxy->total_forces_same_step())) {
-    // e.g. in NAMD, total forces will be available for next timestep
-    // hence we store the current colvar bin
-    force_bin = bin;
-  }
+  // In some cases, total forces are stored for next timestep
+  // hence we store the current colvar bin - this is overwritten on a per-colvar basis
+  // at the top of update()
+  force_bin = bin;
 
 
   // ******************************************************************
